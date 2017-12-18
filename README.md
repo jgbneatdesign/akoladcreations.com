d46ced1283216a309412fdaec14f2c348a5bfd50
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2017-12-18 17:22:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Akolad Creations
+
+This repository hold the source code for the **Akolad Creations** website.
+It's built on **Jekyll** and hosted on Netlify via GitHub.
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var elixir = require('laravel-elixir');
+
+elixir.config.assetsPath = 'assets/source';
+elixir.config.publicPath = 'assets';
+
+elixir(function(mix) {
+	mix
+		.less('app.less')
+
+		.scripts('site.js')
+
+		// .combine([
+		// 	// 'node_modules/countdownCube-master/countdowncube.min.css',
+		// 	'assets/source/vendor/tutorialzine-countdown/assets/countdown/jquery.countdown.css',
+		// ], 'assets/css/vendor.css')
+
+		.combine([
+			'node_modules/jquery/dist/jquery.min.js',
+			'node_modules/bootstrap/dist/js/bootstrap.min.js',
+			'node_modules/jquery.easing/jquery.easing.min.js',
+			// 'node_modules/countdownCube-master/countdowncube.js',
+			// 'assets/source/vendor/tutorialzine-countdown/assets/countdown/jquery.countdown.js'
+		], 'assets/js/vendor.js');
+});
